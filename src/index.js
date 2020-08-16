50601367275b66654c342457cda05f4765c8ad30
al <a.k.sugiharto@gmail.com>
2020-08-16 13:00:00 +0200

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Routes from "./routes";
-//import registerServiceWorker from "./registerServiceWorker";
-
-ReactDOM.render(<Routes />, document.getElementById("root"));
-//registerServiceWorker();
